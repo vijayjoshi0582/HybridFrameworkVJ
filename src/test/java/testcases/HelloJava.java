a294a81063afb2547c6a5227c0a192539deaa410
vijay <vijayjoshi0582@gmail.com>
2023-07-28 16:03:22 +0530

    added hello java

---
+++ src/test/java/testcases/HelloJava.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class HelloJava {
+	
+	@Test
+	public void helloWorld(){
+		
+		System.out.println("Inside helloWorld method()");
+		
+		
+	}
+
+}
