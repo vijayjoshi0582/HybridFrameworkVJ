c6e6ce23527e15cf726db06226305428497c4e83
vijay <vijayjoshi0582@gmail.com>
2023-07-28 15:30:40 +0530

    first commit

---
+++ src/test/java/testcases/UserLoginPage.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class UserLoginPage {
+	
+	@Test
+	public void loginTest(){
+		
+		System.out.println("Inside user login method()");
+	}
+
+}
